c3ecf36dd66fac1ee67ec3d7033661f1fa4ace82
tbricca <tbricca@gmail.com>
2018-07-17 15:36:18 -0700

    added user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const router = express.Router();
 
+// @route           GET REQUEST api/users/test
+// @description     TESTS USERS ROUTE
+// @access          Public
+
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
 module.exports = router;
