039a415870534170fcdbeb777652f13f470d99d6
tbricca <tbricca@gmail.com>
2018-07-17 14:56:19 -0700

    added additional folder and comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+// @route           GET REQUEST api/posts/test
+// @description     TESTS POST ROUTE
+// @access          Public
+
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const router = express.Router();
 
+// @route           GET REQUEST api/profile/profile
+// @description     TESTS PROFILE ROUTE
+// @access          Public
+
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
 module.exports = router;
