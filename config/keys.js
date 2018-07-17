4f742c5d151b04520540b34ddeda280df02f3129
tbricca <tbricca@gmail.com>
2018-07-17 13:17:43 -0700

    connected it to MongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+// making this object available outside of this file
+module.exports = {
+  mongoURI: "mongodb://tony:toeknee33@ds027789.mlab.com:27789/tonyconnector"
+};
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 // initalize a variable called app to express
 const app = express();
+
+// DB CONFIG
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB has been Connected"))
+  .catch(err => console.log(err));
 
 // route to get something up and running
 app.get("/", (req, res) => res.send("Hello there"));
