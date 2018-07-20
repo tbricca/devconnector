cabffe8a9a2c00c8b40cc864f6fc9c6b7898b9db
tbricca <tbricca@gmail.com>
2018-07-20 13:20:16 -0700

    added schema for adding a new user, checking if they already have an account, and hashing the password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,14 @@
 // login, authentication, etc
 const express = require("express");
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 // Load User model
 const User = require('../../models/User');
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // @route           GET REQUEST api/users/test
 // @description     TESTS USERS ROUTE
@@ -15,7 +20,9 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @description     Register User
 // @access          Public
 router.post("/register", (req, res) => res.json({ 
-    
+    //searching for the email that the user is trying to register with
+    //to use req.body we need to bring in Body Parser
+    User.findOne({ email: req.body.email })
     msg: "New User Added"
 });
 
