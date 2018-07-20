e3246c8985de96c68c63b4590f555c26813fd93b
tbricca <tbricca@gmail.com>
2018-07-20 12:54:20 -0700

    started to work on adding be able to create a new user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,21 @@
 const express = require("express");
 const router = express.Router();
 
+// Load User model
+const User = require('../../models/User');
+
 // @route           GET REQUEST api/users/test
 // @description     TESTS USERS ROUTE
 // @access          Public
 
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+// @route           GET REQUEST api/users/register
+// @description     Register User
+// @access          Public
+router.post("/register", (req, res) => res.json({ 
+    
+    msg: "New User Added"
+});
 
 module.exports = router;
