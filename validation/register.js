a469c402841d8e71935913bb4331a027263a321c
tbricca <tbricca@gmail.com>
2018-08-01 09:23:17 -0700

    working on adding validation for users when they register, fixed bugs and its working. Will need to add more before validation is complete

---
+++ validation/register.js
@@ -0,0 +1,17 @@
+const validator = require("validator");
+import isEmpty from "./is-empty";
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  if (!Valiator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
+
+// problem here, ; is expected fix it
