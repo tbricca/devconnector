44b303b9fd53ec1ad76508cfd513f7f1ad1413d0
tbricca <tbricca@gmail.com>
2018-08-01 10:01:27 -0700

    updated email error syntax

--- routes/api/users.js
+++ routes/api/users.js
@@ -36,7 +36,8 @@ router.post("/register", (req, res) => {
   //to use req.body we need to bring in Body Parser
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // Size
